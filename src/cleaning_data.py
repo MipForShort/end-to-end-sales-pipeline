72c9719a33b5935e2959a5f29b1c1d227d73a645
Mario Meade <mariomip98@gmail.com>
2026-06-30 13:42:37 -0600

    feat: Added a pdf extract method and a web scraping method

--- src/cleaning_data.py
+++ src/cleaning_data.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import os
 import glob
-from sqlalchemy import create_engine
-from dotenv import load_dotenv
+import tabula
+import requests
+from bs4 import BeautifulSoup
+#from sqlalchemy import create_engine
+#from dotenv import load_dotenv
 
 #load_dotenv(encoding='utf-8')
 
@@ -10,7 +13,9 @@ CONFIG = {
     'ventas': './data/VPD/ventas_*.csv',
     'clientes': './data/clientes.csv',
     'productos': './data/productos.xlsx',
-    'sucursales': './data/sucursales.csv'
+    'sucursales': './data/sucursales.csv',
+    'productos_proveedor': './data/productos_proveedor.pdf',
+    'pais_url': './data/url.txt'
 }
 
 
@@ -71,6 +76,7 @@ def dataframe_clientes(ruta):
     
     df = pd.read_csv(ruta)
 
+    # Renombramos las columnas
     df.rename(columns={'Id': 'id',
                        'Nombre': 'nombre',
                        'Apellido': 'apellido',
@@ -95,6 +101,7 @@ def dataframe_productos(ruta):
 
     df = pd.read_excel(ruta, engine='openpyxl', sheet_name='productos')
 
+    # Renombramos las columnas 
     df.rename(columns={'Id': 'id',
                        'Nombre': 'nombre',
                        'Descripción': 'descripcion',
@@ -113,6 +120,7 @@ def dataframe_proveedores(ruta):
 
     df = pd.read_excel(ruta, engine='openpyxl', sheet_name='proveedores')
 
+    # Renombramos las columnas
     df.rename(columns={'Id': 'id',
                        'Nombre': 'nombre',
                        'Contacto': 'contacto',
@@ -133,6 +141,7 @@ def dataframe_sucursales(ruta):
 
     df = pd.read_csv(ruta)
 
+    # Renombramos las columnas
     df.rename(columns={'dirección': 'direccion'}, inplace=True)
 
     # Separamos la columna ubicacion en tres columnas distintas
@@ -152,19 +161,108 @@ def dataframe_sucursales(ruta):
     return df
 
 
+def dataframe_productos_proveedor(ruta):
+
+    # Leemos un archivo pdf con la ruta de nuestro CONFIG
+    table = tabula.read_pdf(ruta, pages="all")
+    # Esta linea hace nuestro dataframe de la primer tabla que encontramos (la unica)
+    df = table[0]
+
+    # Renombramos las columnas
+    df.rename(columns={'días_entrega': 'dias_entrega'}, inplace=True)
+
+    # Revisamos si hay filas duplicadas
+    #filas_duplicadas = df.duplicated().sum()
+    #print(f"Filas duplicadas: {filas_duplicadas}")
+
+    return df
+
+
+def dataframe_pais_url(ruta):
+
+    # Abrimos el archivo url.text de nuestra data y leemos el url
+    with open(ruta, "r") as file:
+        line = file.readline()
+        #print(line)
+
+    url = line
+    #print(url)
+
+    # Revisamos que el modulo requests de una respuesta de [200]
+    r = requests.get(url)
+    #print(r)
+
+    # Con el request creamos una sopa que es muy hermosa
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    # Ahora con nuestra sopa vamos a encontrar una tabla con un clase especifica
+    table = soup.find("table", class_="table table-bordered downloads tablesorter")
+
+    # Este es el proceso para separar los header de nuestra tabla
+    headers = table.find_all("th")
+    #print(headers)
+
+    titles = []
+    for i in headers:
+        titles.append(i.text)
+    #print(titles)
+
+    # Con los header listos, creamos un dataframe
+    df = pd.DataFrame(columns=titles)
+
+    # Este es el proceso para conseguir la data de la tabla
+    # Revisamos todos los tr de la tabla
+    rows = table.find_all("tr")
+
+    # Como los headers tambien son tr, empezamos desde el indice 1 hasta el final nuestro for
+    # Luego buscamos todos los td y los asignaremos el td.text a una variable row para despues
+    # agregarla a nuestro dataframe
+    for i in rows[1:]:
+        data = i.find_all("td")
+        row = [td.text for td in data]
+        #print(row)
+        df.loc[len(df)] = row
+
+    # Eliminamos las tablas que no ocupamos
+    df = df.drop(columns=['Alpha-3 code', 'Numeric'])
+
+    # Renombramos las columnas
+    df.rename(columns={'Country': 'pais',
+                       'Alpha-2 code': 'codigo'}, inplace=True)
+
+    # Revisamos si hay filas duplicadas
+    #filas_duplicadas = df.duplicated().sum()
+    #print(f"Filas duplicadas: {filas_duplicadas}")
+
+    return df
+
 if __name__ == '__main__':
     
     ventas = dataframe_ventas(CONFIG['ventas'])
-    print(ventas.info())
+    #print(ventas.info())
+    #print(ventas.head())
 
     clientes = dataframe_clientes(CONFIG['clientes'])
-    print(clientes.info())
+    #print(clientes.info())
+    #print(clientes.head())
 
     productos = dataframe_productos(CONFIG['productos'])
-    print(productos.info())
+    #print(productos.info())
+    #print(productos.head())
 
     proveedores = dataframe_proveedores(CONFIG['productos'])
-    print(proveedores.info())
+    #print(proveedores.info())
+    #print(proveedores.head())
 
     sucursales = dataframe_sucursales(CONFIG['sucursales'])
-    print(sucursales.info())
+    #print(sucursales.info())
+    #print(sucursales.head())
+
+    productos_proveedor = dataframe_productos_proveedor(CONFIG['productos_proveedor'])
+    #print(productos_proveedor.info())
+    #print(productos_proveedor.head())
+
+    pais = dataframe_pais_url(CONFIG['pais_url'])
+    #print(pais.info())
+    #print(pais.head())
+
